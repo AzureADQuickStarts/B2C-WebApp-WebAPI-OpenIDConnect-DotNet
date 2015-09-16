3c3b77a9584ebc886ea36468d6546db2df922b33
dstrockis <dastrock@microsoft.com>
2015-09-16 13:35:12 -0700

    Ripping out identity pieces

--- TaskWebApp/Controllers/TasksController.cs
+++ TaskWebApp/Controllers/TasksController.cs
@@ -25,60 +25,9 @@ namespace TaskService.Controllers
         // GET: TodoList
         public async Task<ActionResult> Index()
         {
-            AuthenticationResult result = null;
-            try
-            {
-                string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-                string authority = String.Format(CultureInfo.InvariantCulture, Startup.aadInstance, Startup.tenant, string.Empty, string.Empty);
-                ClientCredential credential = new ClientCredential(Startup.clientId, Startup.clientSecret);
+            // TODO: Get a token from the ADAL cache
 
-                // We don't care which policy is used to access the TaskService, so let's use the most recent policy
-                string mostRecentPolicy = ClaimsPrincipal.Current.FindFirst(Startup.AcrClaimType).Value;
-                
-                // Here you ask for a token using the web app's clientId as the scope, since the web app and service share the same clientId.
-                // AcquireTokenSilentAsync will return a token from the token cache, and throw an exception if it cannot do so.
-                AuthenticationContext authContext = new AuthenticationContext(authority, new NaiveSessionCache(userObjectID));
-                result = await authContext.AcquireTokenSilentAsync(new string[] { Startup.clientId }, credential, UserIdentifier.AnyUser, mostRecentPolicy);
-
-                HttpClient client = new HttpClient();
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, serviceUrl + "/api/tasks");
-
-                // Add the token acquired from ADAL to the request headers
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
-                HttpResponseMessage response = await client.SendAsync(request);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    String responseString = await response.Content.ReadAsStringAsync();
-                    JArray tasks = JArray.Parse(responseString);
-                    ViewBag.Tasks = tasks;
-                    return View();
-                }
-                else
-                {
-                    // If the call failed with access denied, then drop the current access token from the cache, 
-                    // and show the user an error indicating they might need to sign-in again.
-                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    {
-                        var todoTokens = authContext.TokenCache.ReadItems().Where(a => a.Scope.Contains(Startup.clientId));
-                        foreach (TokenCacheItem tci in todoTokens)
-                            authContext.TokenCache.DeleteItem(tci);
-
-                        return new RedirectResult("/Error?message=Error: " + response.ReasonPhrase + " You might need to sign in again.");
-                    }
-                }
-
-                return new RedirectResult("/Error?message=An Error Occurred Reading To Do List: " + response.StatusCode);
-            }
-            catch (AdalException ee)
-            {
-                // If ADAL could not get a token silently, show the user an error indicating they might need to sign in again.
-                return new RedirectResult("/Error?message=An Error Occurred Reading To Do List: " + ee.Message + " You might need to log out and log back in.");
-            }
-            catch (Exception ex)
-            {
-                return new RedirectResult("/Error?message=An Error Occurred Reading To Do List: " + ex.Message);
-            }
+            // TODO: Get tasks from the TaskService
         }
 
         // POST: TodoList/Create
@@ -144,18 +93,13 @@ namespace TaskService.Controllers
 
             try
             {
-                string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
-                string authority = String.Format(CultureInfo.InvariantCulture, Startup.aadInstance, Startup.tenant, string.Empty, string.Empty);
-                ClientCredential credential = new ClientCredential(Startup.clientId, Startup.clientSecret);
-
-                string mostRecentPolicy = ClaimsPrincipal.Current.FindFirst(Startup.AcrClaimType).Value;
-
-                AuthenticationContext authContext = new AuthenticationContext(authority, new NaiveSessionCache(userObjectID));
-                result = await authContext.AcquireTokenSilentAsync(new string[] { Startup.clientId }, credential, UserIdentifier.AnyUser, mostRecentPolicy);
+                // TODO: Get a token from ADAL
 
                 HttpClient client = new HttpClient();
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Delete, serviceUrl + "/api/tasks/" + id);
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", result.Token);
+                
+                // TODO: Attach the token to the HTTP DELETE request
+
                 HttpResponseMessage response = await client.SendAsync(request);
 
                 if (response.IsSuccessStatusCode)
